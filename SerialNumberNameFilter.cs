73b9a4e1a4cf935b0ad6e1ba36b409eeac08be57
Patrick Foley <patrickfoley@live.com>
2016-05-05 08:53:56 -0400

    Update SerialNumberNameFilter.cs

--- SerialNumberNameFilter.cs
+++ SerialNumberNameFilter.cs
@@ -1,4 +1,9 @@
 // The internal "serial number id" is the number that shows up on the address bar/url.
+// ... so for the URL https://patrick.moraware.net/d.aspx?wp=37&snId=1871
+// ... the serial number id is 1871, which you can get directly with 
+            var id = 1871;
+            var sn = conn.GetSerialNumber(id);
+
 // The "serial number name" is what most people would think of as the serial number.
 // To find by "serial number name" you have to search for it, which is slightly cumbersome:
 
