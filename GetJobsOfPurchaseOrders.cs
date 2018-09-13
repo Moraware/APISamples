f4dc702e8748d8cbfb1dbab7cf3f87434f56aca2
Patrick Foley <patrickfoley@live.com>
2018-09-13 16:58:46 -0400

    Create GetJobsOfPurchaseOrders.cs

---
+++ GetJobsOfPurchaseOrders.cs
@@ -0,0 +1,21 @@
+            // Example of using GetJobsOfPurchaseOrders
+            // This method exists starting in Version 5 of the JobTracker API
+            // Prior to Version 5, there was no way to get this information from the API
+
+            // var DB = "yourdatabasename";
+            var JTURL = "https://" + DB + ".moraware.net/";
+            var UID = "api";
+            // var PWD = "your password";
+
+            Connection conn = new Connection(JTURL + "api.aspx", UID, PWD);
+            conn.Connect();
+
+            int[] poIds = new int[] {3756, 3754, 3703}; // Random POs from my database. Yours will be different
+
+            var jobs = conn.GetJobsOfPurchaseOrders(poIds);
+            foreach (var job in jobs)
+            {
+                Console.WriteLine("Job ID: {0} | Job Name: {1}", job.JobId, job.JobName);
+            }
+
+            conn.Disconnect();
