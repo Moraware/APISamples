002b9cd1b81d0f53175e8828b95b7a7548322539
Patrick Foley <patrickfoley@live.com>
2019-02-26 16:08:35 -0500

    Update CreatePhaseAndActivity.cs
    Get rid of tabs

--- V4Samples/CreatePhaseAndActivity.cs
+++ V4Samples/CreatePhaseAndActivity.cs
@@ -1,17 +1,17 @@
-            conn.Connect();
+conn.Connect();
 
-            int nActivityType = 256; // an activity type in my database - yours will be completely different
-            /////////////////////////// call conn.GetJobActivityTypes to find them
-            int nActivityStatus = 3; // an activity status in my database - yours will be completely different
-            /////////////////////////// call conn.GetJobActivityStatuses to find them
-            int nJob = 6490;        // a random job in my database - yours will be completely different
+int nActivityType = 256; // an activity type in my database - yours will be completely different
+/////////////////////////// call conn.GetJobActivityTypes to find them
+int nActivityStatus = 3; // an activity status in my database - yours will be completely different
+/////////////////////////// call conn.GetJobActivityStatuses to find them
+int nJob = 6490;        // a random job in my database - yours will be completely different
 
-            // Create a job phase - note that you need the job id but not the Job object
-            // To use an existing phase instead, get the Job object and figure out which phase you want from job.JobPhases
+// Create a job phase - note that you need the job id but not the Job object
+// To use an existing phase instead, get the Job object and figure out which phase you want from job.JobPhases
 
-            var jp = new JobPhase(nJob, "Phase 2");
-            conn.CreateJobPhase(jp); 
+var jp = new JobPhase(nJob, "Phase 2");
+conn.CreateJobPhase(jp); 
 
-            var ja = new JobActivity(nJob, nActivityType, nActivityStatus);
-            ja.JobPhases.Add(jp);
-            conn.CreateJobActivity(ja);
+var ja = new JobActivity(nJob, nActivityType, nActivityStatus);
+ja.JobPhases.Add(jp);
+conn.CreateJobActivity(ja);
